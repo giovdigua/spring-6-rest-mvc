adf1513623d0260e0780680aa5702419ea3bcaf8
Giovanni DI GUARDO <g.diguardo@cineca.it>
2024-05-11 11:52:06 +0200

    Project Lombok Constructor

---
+++ src/main/java/com/giovdigua/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,14 @@
+package com.giovdigua.spring6restmvc.controller;
+
+import com.giovdigua.spring6restmvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@AllArgsConstructor
+@Controller
+public class BeerController {
+
+    private final BeerService beerService;
+
+
+}
