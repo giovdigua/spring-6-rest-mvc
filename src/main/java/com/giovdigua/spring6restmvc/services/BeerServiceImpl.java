4ec1806c575fd6062407df4bd33f6c248550f240
Giovanni DI GUARDO <g.diguardo@cineca.it>
2024-05-11 12:15:10 +0200

    Project Lombok Logging

--- src/main/java/com/giovdigua/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/com/giovdigua/spring6restmvc/services/BeerServiceImpl.java
@@ -2,14 +2,21 @@ package com.giovdigua.spring6restmvc.services;
 
 import com.giovdigua.spring6restmvc.model.Beer;
 import com.giovdigua.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by Id - in service. Id: " + id.toString());
+
         return Beer.builder()
                 .id(id)
                 .version(1)
