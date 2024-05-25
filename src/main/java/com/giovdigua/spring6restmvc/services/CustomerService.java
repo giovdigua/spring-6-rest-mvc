dc65c29778e9e57848477640e90fc954d23cb79e
Giovanni DI GUARDO <g.diguardo@cineca.it>
2024-05-25 18:26:28 +0200

    Assignment - GET Customer

---
+++ src/main/java/com/giovdigua/spring6restmvc/services/CustomerService.java
@@ -0,0 +1,14 @@
+package com.giovdigua.spring6restmvc.services;
+
+import com.giovdigua.spring6restmvc.model.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerService {
+
+    List<Customer> getAllCustomers();
+
+    Customer getCustomerById(UUID id);
+}
